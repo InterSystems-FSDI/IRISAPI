e6d225e3add675b44f5f9e5d18ce27e0a5fd082d
JingyuMarcelLee <leemar@dickinson.edu>
2023-06-05 10:24:49 -0400

    added pyodbc driver, fixed README and app.py

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 Mini Project for IRIS API
 
 - add, update, and delete from Demo.Person 
+
+## PYODBC (SQL)
+1. Install InterSystems IRIS Native API for Python, following the directions below for your operating system. Note that InterSystems Learning Labs, AWS, GCP, and Azure all use Linux.
+
+- Windows: 
+    ```  
+    pip install nativeAPI_wheel\irisnative-1.0.0-cp34.cp35.cp36.cp37.cp38.cp39-none-win_amd64.whl 
+    ```
+
+- macOS: 
+    ```  
+    pip istall nativeAPI_wheel/irisnative-1.0.0-cp34-abi3-macosx_10_13_x86_64.macosx_10_14_x86_64.whl
+    ```
+- Linux: 
+    ```
+    pip install nativeAPI_wheel/irisnative-1.0.0-cp34-abi3-linux_x86_64.whl
+    ```
+2. Install InterSystems IRIS PyODBC drive
+- Windows: 
+  ```
+  pyodbc_wheel\ODBC-2022.1.0.209.0-win_x64.exe
+  ```
+- macOS: 
+  ```
+  odbcinst –i –d –f pyodbc_wheel/mac/odbcinst.ini
+  ```
+- Linux: 
+  ```
+  sudo odbcinst -i -d -f pyodbc_wheel/linux/odbcinst.ini
+  ```
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import iris
+import pyodbc
 
 # Get connection details from config file
-def get_connection_info(file_name):
+def get_connection_info(file_name: str) -> dict:
     # Initial empty dictionary to store connection details
     connections = {}
 
@@ -17,7 +18,7 @@ def get_connection_info(file_name):
             connections[connection_param] = connection_value
     return connections
 
-def run():
+def run() -> None:
     # Retrieve connection information from configuration file
     connection_detail = get_connection_info("connection.config")
 
@@ -26,11 +27,20 @@ def run():
     namespace = connection_detail["namespace"]
     username = connection_detail["username"]
     password = connection_detail["password"]
-
-    # Create connection to InterSystems IRIS
+    driver = "{InterSystems IRIS ODBC35}"
+    
+    # Create connection to InterSystems IRIS using native API
     connection = iris.connect(ip, port, namespace, username, password)
 
-    print("Connected to InterSystems IRIS")
+    print("Connected to InterSystems IRIS using Native API")
+    
+    # Create connection to InterSystems IRIS using pyodbc
+    connection_string = 'DRIVER={};SERVER={};PORT={};DATABASE={};UID={};PWD={}' \
+    .format(driver, ip, port, namespace, username, password)
+    connection_pyodbc = pyodbc.connect(connection_string)
+    connection_pyodbc.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+    connection_pyodbc.setencoding(encoding='utf-8')
+    print("Connected to InterSystems IRIS using pyodbc")
 
     # Create an iris object
     iris_obj = iris.createIRIS(connection)
